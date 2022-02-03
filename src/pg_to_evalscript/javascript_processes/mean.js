2bbaefa719bdda49dd5761896caaa8d5aff16ee3
Zan Pecovnik <zan.pecovnik@sinergise.com>
2022-02-03 17:22:14 +0100

    Fix mean function and add test cases

--- src/pg_to_evalscript/javascript_processes/mean.js
+++ src/pg_to_evalscript/javascript_processes/mean.js
@@ -1,4 +1,14 @@
 function mean(arguments) {
-    const {data} = arguments;
-    return data.reduce((prev, curr) => prev + curr)/data.length
+  const { data, ignore_nodata = true } = arguments;
+  if (
+    (!ignore_nodata && data.includes(null)) ||
+    data.length === 0 ||
+    data.every((i) => i === null)
+  ) {
+    return null;
+  }
+  return (
+    data.reduce((prev, curr) => prev + curr) /
+    data.filter((i) => i !== null).length
+  );
 }
--- tests/unit_tests/test_mean.py
+++ tests/unit_tests/test_mean.py
@@ -12,9 +12,26 @@ def mean_process_code():
 
 @pytest.mark.parametrize(
     "example_input,expected_output",
-    [([1, 3, 5, 7], 4), ([6, 6], 6), ([1, 0, 3, 2], 1.5)],
+    [
+        ({'data': [1, 3, 5, 7]}, 4), 
+        ({'data': [6, 6, 6]}, 6), 
+        ({'data': [1, 0, 3, 2]}, 1.5), 
+        ({'data': [1, 3, 5, 7], 'ignore_nodata': False}, 4),
+        ({'data': [0, 0, 0, 0, 0], 'ignore_nodata': False}, 0),
+        ({'data': [0, 0, 0, 0, 0]}, 0), 
+        ({'data': [1, None], 'ignore_nodata': False}, None), 
+        ({'data': [1, None]}, 1),
+        ({'data': [9, 2.5, None, -2.5]}, 3),
+        ({'data': [9, 2.5, None, -2.5], 'ignore_nodata': False}, None),
+        ({'data': []}, None),
+        ({'data': [], 'ignore_nodata': False}, None),
+        ({'data': [None]}, None),
+        ({'data': [None], 'ignore_nodata': False}, None),
+        ({'data': [None, None, None]}, None),
+        ({'data': [1, None, -3, -18, None, None, 27, 3]}, 2)
+    ]
 )
 def test_mean(mean_process_code, example_input, expected_output):
-    output = run_process(mean_process_code, "mean", {"data": example_input})
+    output = run_process(mean_process_code, "mean", example_input)
     output = json.loads(output)
     assert output == expected_output
