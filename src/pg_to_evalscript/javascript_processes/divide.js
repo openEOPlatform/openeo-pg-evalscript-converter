101f13b11a38e7a3f56506ee0bf8a1d30b95e580
Zan Pecovnik <zan.pecovnik@sinergise.com>
2022-02-04 12:21:48 +0100

    Fix divide process and add test cases

--- src/pg_to_evalscript/javascript_processes/divide.js
+++ src/pg_to_evalscript/javascript_processes/divide.js
@@ -1,4 +1,7 @@
 function divide(arguments) {
-    const {x, y} = arguments;
-    return x / y
+  const { x, y } = arguments;
+  if (x === null || y === null) {
+    return null;
+  }
+  return x / y;
 }
---
+++ tests/unit_tests/test_divide.py
@@ -0,0 +1,28 @@
+import json
+
+import pytest
+
+from tests.utils import load_process_code, run_process
+
+
+@pytest.fixture
+def divide_process_code():
+    return load_process_code("divide")
+
+
+@pytest.mark.parametrize(
+    "example_input,expected_output",
+    [
+        ({'x': 1, 'y': 2}, 0.5),
+        ({'x': None, 'y': 3}, None),
+        ({'x': 1, 'y': None}, None),
+        ({'x': 0, 'y':1}, 0),
+        ({'x': -1, 'y':8}, -0.125),
+        ({'x': 23, 'y':-26}, -0.88461538461),
+        ({'x': -3, 'y':-11}, 0.27272727272)
+    ],
+)
+def test_divide(divide_process_code, example_input, expected_output):
+    output = run_process(divide_process_code, "divide", example_input)
+    output = json.loads(output)
+    assert pytest.approx(output) == expected_output
