223feeaa7771bf490fa99fae955464224680c3fc
Zan Pecovnik <zan.pecovnik@sinergise.com>
2022-02-04 13:03:28 +0100

    Throw exception when dividing by zero

--- src/pg_to_evalscript/javascript_processes/divide.js
+++ src/pg_to_evalscript/javascript_processes/divide.js
@@ -1,5 +1,8 @@
 function divide(arguments) {
   const { x, y } = arguments;
+  if (y === 0) {
+    throw "Division by zero is not supported.";
+  }
   if (x === null || y === null) {
     return null;
   }
