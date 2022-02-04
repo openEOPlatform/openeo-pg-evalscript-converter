d2d580808f0c3d4cc21205efe9373b38ff84164a
Zan Pecovnik <zan.pecovnik@sinergise.com>
2022-02-04 12:22:15 +0100

    Fix subtract process and add test cases

--- src/pg_to_evalscript/javascript_processes/subtract.js
+++ src/pg_to_evalscript/javascript_processes/subtract.js
@@ -1,4 +1,7 @@
 function subtract(arguments) {
-    const {x, y} = arguments;
-    return x - y
+  const { x, y } = arguments;
+  if (x === null || y === null) {
+    return null;
+  }
+  return x - y;
 }
---
+++ tests/unit_tests/test_subtract.py
@@ -0,0 +1,31 @@
+import json
+
+import pytest
+
+from tests.utils import load_process_code, run_process
+
+
+@pytest.fixture
+def subtract_process_code():
+    return load_process_code("subtract")
+
+
+@pytest.mark.parametrize(
+    "example_input,expected_output",
+    [
+        ({'x':1,  'y': 2}, -1),
+        ({'x':None,  'y': 3}, None),
+        ({'x':1,  'y': None}, None),
+        ({'x':-2,  'y': 26}, -28),
+        ({'x':-3,  'y': -8}, 5),
+        ({'x':0,  'y': -6}, 6),
+        ({'x':-23,  'y': 0}, -23),
+        ({'x':0,  'y': 0}, 0),
+        ({'x':2.3,  'y': -4.233}, 6.533),
+        ({'x':-23.84,  'y': -4.21}, -19.63)
+    ],
+)
+def test_subtract(subtract_process_code, example_input, expected_output):
+    output = run_process(subtract_process_code, "subtract", example_input)
+    output = json.loads(output)
+    assert pytest.approx(output) == expected_output
