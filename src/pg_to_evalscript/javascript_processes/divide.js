bad3d43e46cb6f93cab609bcba5699dc4a356105
Zan Pecovnik <zan.pecovnik@sinergise.com>
2022-02-09 11:46:24 +0100

    Check if exception is thrown in divide

--- src/pg_to_evalscript/javascript_processes/divide.js
+++ src/pg_to_evalscript/javascript_processes/divide.js
@@ -1,7 +1,15 @@
+function _divide(arguments) {
+  try {
+    return divide(arguments);
+  } catch (e) {
+    return e.message;
+  }
+}
+
 function divide(arguments) {
   const { x, y } = arguments;
   if (y === 0) {
-    throw "Division by zero is not supported.";
+    throw new Error("Division by zero is not supported.");
   }
   if (x === null || y === null) {
     return null;
--- tests/unit_tests/test_divide.py
+++ tests/unit_tests/test_divide.py
@@ -19,10 +19,11 @@ def divide_process_code():
         ({'x': 0, 'y':1}, 0),
         ({'x': -1, 'y':8}, -0.125),
         ({'x': 23, 'y':-26}, -0.88461538461),
-        ({'x': -3, 'y':-11}, 0.27272727272)
+        ({'x': -3, 'y':-11}, 0.27272727272),
+        ({'x': 2, 'y': 0}, 'Division by zero is not supported.')
     ],
 )
 def test_divide(divide_process_code, example_input, expected_output):
-    output = run_process(divide_process_code, "divide", example_input)
+    output = run_process(divide_process_code, "_divide", example_input)
     output = json.loads(output)
     assert pytest.approx(output) == expected_output
