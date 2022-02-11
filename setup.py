097d2c3eb431743c2906062e4e7cc00a6f1fbab2
jjezersek <jan.jezersek@sinergise.com>
2022-02-11 15:24:52 +0100

    Use versiontag to set package version

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Distribution
+
+Distribution().fetch_build_eggs('versiontag')
+
+from versiontag import get_version, cache_git_tag
+
+cache_git_tag()
 
 setup(
     name="pg_to_evalscript",
-    version="0.0.1",
+    version=get_version(pypi=True),
     packages=["pg_to_evalscript"],
     package_data={"pg_to_evalscript": ["javascript_datacube/*.js", "javascript_processes/*.js"]},
 )
