80fe5e5b89ec7db996623e0f3cc84912fc265de3
Damjan Gostenčnik <damjan.gostencnik@gmail.com>
2022-02-14 15:04:01 +0100

    add intergration test for apply

--- tests/integration_tests/test_integration.py
+++ tests/integration_tests/test_integration.py
@@ -11,6 +11,7 @@ from tests.utils import get_process_graph_json, run_evalscript
     [
         ("test_graph_1", [{"B01": 3, "B02": 3}, {"B01": 5, "B02": 1}], [4, 2]),
         ("test_short_graph", [{"B01": 3, "B02": 2}, {"B01": 5, "B02": 1}], [3, 2, 5, 1]),
+        ("test_apply_absolute", [{"B01": 0.1, "B02": -0.15}, {"B01": 0, "B02": 2}], [0.1, 0.15, 0, 2]),
     ],
 )
 def test_convertable_process_graphs(pg_name, example_input, expected_output):
