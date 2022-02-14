8fd794a0edaa872b25594233a88c8279d4cc4b22
Zan Pecovnik <zan.pecovnik@sinergise.com>
2022-02-14 10:26:37 +0100

    Fix process and add additional test cases

--- src/pg_to_evalscript/javascript_processes/array_concat.js
+++ src/pg_to_evalscript/javascript_processes/array_concat.js
@@ -1,4 +1,14 @@
 function array_concat(arguments) {
   const { array1, array2 } = arguments;
+  if (array1 === null || array1 === undefined) {
+    throw new Error(
+      "Mandatory argument `array1` is either null or not defined."
+    );
+  }
+  if (array2 === null || array2 === undefined) {
+    throw new Error(
+      "Mandatory argument `array2` is either null or not defined."
+    );
+  }
   return [...array1, ...array2];
 }
--- tests/unit_tests/test_array_concat.py
+++ tests/unit_tests/test_array_concat.py
@@ -43,3 +43,22 @@ def test_array_concat(array_concat_process_code, example_input, expected_output)
     output = run_process(array_concat_process_code, "array_concat", example_input)
     output = json.loads(output)
     assert output == expected_output
+
+@pytest.mark.parametrize(
+    "example_input,raises_exception,error_message",
+    [
+        ({'array1': [1,2,3], 'array2': [4,5,6]}, False, None),
+        ({'array2': [1,2,3]}, True, 'Mandatory argument `array1` is either null or not defined.'),
+        ({'array1': None, 'array2': [1,2,3]}, True, 'Mandatory argument `array1` is either null or not defined.'),
+        ({'array1': [1,2,3]}, True, 'Mandatory argument `array2` is either null or not defined.'),
+        ({'array1': [1,2,3], 'array2': None}, True, 'Mandatory argument `array2` is either null or not defined.'),
+    ]
+)
+def test_array_concat_exceptions(array_concat_process_code, example_input, raises_exception, error_message):
+    if raises_exception:
+        with pytest.raises(Exception) as exc:
+            run_process(array_concat_process_code, "array_concat", example_input)
+        assert error_message in str(exc.value)
+
+    else:
+        run_process(array_concat_process_code, "array_concat", example_input)
