577971ba6c99dac8c628646d184f025a685f348f
Zan Pecovnik <zan.pecovnik@sinergise.com>
2022-02-15 12:32:49 +0100

    Handle all null values in max process

--- src/pg_to_evalscript/javascript_processes/max.js
+++ src/pg_to_evalscript/javascript_processes/max.js
@@ -1,6 +1,10 @@
 function max(arguments) {
   const { data, ignore_nodata = true } = arguments;
-  if ((!ignore_nodata && data.includes(null)) || data.length === 0) {
+  if (
+    (!ignore_nodata && data.includes(null)) ||
+    data.length === 0 ||
+    data.every((x) => x === null)
+  ) {
     return null;
   }
   return Math.max(...data.filter((i) => i !== null));
