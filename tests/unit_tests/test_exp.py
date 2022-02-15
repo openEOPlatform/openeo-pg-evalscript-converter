6e7585b660183f2f419e14b8340ce51958e6b057
Zan Pecovnik <zan.pecovnik@sinergise.com>
2022-02-15 14:23:53 +0100

    Add additional test case and run black

--- tests/unit_tests/test_exp.py
+++ tests/unit_tests/test_exp.py
@@ -13,13 +13,13 @@ def exp_process_code():
 @pytest.mark.parametrize(
     "example_input,expected_output",
     [
-        ({'p': 1}, 2.718281828459045),
-        ({'p': 2.718281828459045}, 15.154262241479262),
-        ({'p': None}, None),
-        ({'p': 2.3}, 9.974182454814718),
-        ({'p': -1.2}, 0.3011942119122021),
-        ({'p': -6.9}, 0.0010077854290485105),
-        ({'p': 0}, 1)
+        ({"p": 1}, 2.718281828459045),
+        ({"p": 2.718281828459045}, 15.154262241479262),
+        ({"p": None}, None),
+        ({"p": 2.3}, 9.974182454814718),
+        ({"p": -1.2}, 0.3011942119122021),
+        ({"p": -6.9}, 0.0010077854290485105),
+        ({"p": 0}, 1),
     ],
 )
 def test_exp(exp_process_code, example_input, expected_output):
@@ -27,15 +27,16 @@ def test_exp(exp_process_code, example_input, expected_output):
     output = json.loads(output)
     assert output == expected_output
 
+
 @pytest.mark.parametrize(
     "example_input,raises_exception,error_message",
     [
-        ({'p': 1}, False, None),
-        ({}, True, 'Mandatory argument `p` is not defined.'),
-        ({'q': 0.5}, True, 'Mandatory argument `p` is not defined.'),
-        ({'p': '0.5'}, True, 'Argument `p` is not a number.'),
-        ({'p': {'p': 0.5}}, True, 'Argument `p` is not a number.')
-    ]
+        ({"p": 1}, False, None),
+        ({}, True, "Mandatory argument `p` is not defined."),
+        ({"q": 0.5}, True, "Mandatory argument `p` is not defined."),
+        ({"p": "0.5"}, True, "Argument `p` is not a number."),
+        ({"p": {"p": 0.5}}, True, "Argument `p` is not a number."),
+    ],
 )
 def test_exp_exceptions(exp_process_code, example_input, raises_exception, error_message):
     if raises_exception:
@@ -44,4 +45,4 @@ def test_exp_exceptions(exp_process_code, example_input, raises_exception, error
         assert error_message in str(exc.value)
 
     else:
-        run_process(exp_process_code, "exp", example_input) 
+        run_process(exp_process_code, "exp", example_input)
