ec4b8ecd4bfd5e88409c5e277debb414dee3b7ca
Zan Pecovnik <zan.pecovnik@sinergise.com>
2022-02-15 12:34:34 +0100

    Add additional check to lt process

--- src/pg_to_evalscript/javascript_processes/lt.js
+++ src/pg_to_evalscript/javascript_processes/lt.js
@@ -2,10 +2,12 @@ function lt(arguments) {
   const isValidDate = function (date) {
     return new Date(date) !== "Invalid Date" && !isNaN(new Date(date));
   };
-
   const { x, y } = arguments;
   if (x === null || y === null) {
     return null;
+  }
+  if (typeof x === "object" || typeof y === "object") {
+    return false;
   }
   if (
     typeof x === "string" &&
