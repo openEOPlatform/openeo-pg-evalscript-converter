1e21dc4e78b13651cee4d80eead3a89ddd62030b
Damjan Gostenčnik <damjan.gostencnik@gmail.com>
2022-02-16 11:41:40 +0100

    add tests for lt process

--- src/pg_to_evalscript/javascript_processes/lt.js
+++ src/pg_to_evalscript/javascript_processes/lt.js
@@ -1,4 +1,22 @@
 function lt(arguments) {
-    const {x, y} = arguments;
-    return x < y
+  const { x, y } = arguments;
+  if (x === undefined) {
+    throw Error("Process lt requires argument x.");
+  }
+
+  if (y === undefined) {
+    throw Error("Process lt requires argument y.");
+  }
+
+  if (x === null || y === null) {
+    return null;
+  }
+
+  if (
+    (typeof x !== "number" && typeof x !== "string") ||
+    (typeof y !== "number" && typeof y !== "string")
+  ) {
+    return false;
+  }
+  return x < y;
 }
---
+++ tests/unit_tests/test_lt.py
@@ -0,0 +1,32 @@
+import json
+
+import pytest
+
+from tests.utils import load_process_code, run_process
+
+
+@pytest.fixture
+def lt_code():
+    return load_process_code("lt")
+
+
+@pytest.mark.parametrize(
+    "example_input,expected_output",
+    [
+        ({"x": 1, "y": None}, None),
+        ({"x": 0, "y": 0}, False),
+        ({"x": 1, "y": 2}, True),
+        ({"x": -0.5, "y": -0.6}, False),
+        #  TODO: Temporal strings can not be compared based on their string representation due to the time zone / time-offset representations.
+        ({"x": "00:00:00+01:00", "y": "00:00:00Z"}, True),
+        ({"x": "1950-01-01T00:00:00Z", "y": "2018-01-01T12:00:00Z"}, True),
+        # ({"x": "2018-01-01T12:00:00+00:00", "y": "2018-01-01T12:00:00Z"}, False),
+        #
+        ({"x": 0, "y": True}, False),
+        ({"x": False, "y": True}, False),
+    ],
+)
+def test_lt(lt_code, example_input, expected_output):
+    output = run_process(lt_code, "lt", example_input)
+    output = json.loads(output)
+    assert output == expected_output
