13d2a986ce4df95d1aca0a229731a593303e46c7
Damjan Gostenčnik <damjan.gostencnik@gmail.com>
2022-02-16 11:35:33 +0100

    add gt process

---
+++ tests/unit_tests/test_gt.py
@@ -0,0 +1,32 @@
+import json
+
+import pytest
+
+from tests.utils import load_process_code, run_process
+
+
+@pytest.fixture
+def gt_code():
+    return load_process_code("gt")
+
+
+@pytest.mark.parametrize(
+    "example_input,expected_output",
+    [
+        ({"x": 1, "y": None}, None),
+        ({"x": 0, "y": 0}, False),
+        ({"x": 2, "y": 1}, True),
+        ({"x": -0.5, "y": -0.6}, True),
+        #  TODO: Temporal strings can not be compared based on their string representation due to the time zone / time-offset representations.
+        ({"x": "00:00:00Z", "y": "00:00:00+01:00"}, True),
+        ({"x": "1950-01-01T00:00:00Z", "y": "2018-01-01T12:00:00Z"}, False),
+        ({"x": "2018-01-01T12:00:00+00:00", "y": "2018-01-01T12:00:00Z"}, False),
+        #
+        ({"x": True, "y": 0}, False),
+        ({"x": True, "y": False}, False),
+    ],
+)
+def test_gt(gt_code, example_input, expected_output):
+    output = run_process(gt_code, "gt", example_input)
+    output = json.loads(output)
+    assert output == expected_output
