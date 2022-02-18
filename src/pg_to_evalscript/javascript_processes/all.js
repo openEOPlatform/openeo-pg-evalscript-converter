284b1ab956a52aed705f5ff7fa207e8ff8ea081d
Zan Pecovnik <zan.pecovnik@sinergise.com>
2022-02-18 14:49:42 +0100

    Add all process and test cases

---
+++ src/pg_to_evalscript/javascript_processes/all.js
@@ -0,0 +1,38 @@
+function all(arguments) {
+  const { data, ignore_nodata = true } = arguments;
+
+  if (data === undefined) {
+    throw new Error("Mandatory argument `data` is not defined.");
+  }
+
+  if (!Array.isArray(data)) {
+    throw new Error("Argument `data` is not an array.");
+  }
+
+  if (!data.every((x) => typeof x === "boolean" || x === null)) {
+    throw new Error(
+      "Values in argument `data` can only be of type boolean or null."
+    );
+  }
+
+  if (ignore_nodata) {
+    if (data.filter((x) => x !== null).length === 0) {
+      return null;
+    }
+    return data.filter((x) => x !== null).every((x) => x === true);
+  }
+
+  if (data.length === 0) {
+    return null;
+  }
+
+  return data.reduce((x, y) => {
+    if (x === false || y === false) {
+      return false;
+    }
+    if (x === true && y === true) {
+      return true;
+    }
+    return null;
+  });
+}
