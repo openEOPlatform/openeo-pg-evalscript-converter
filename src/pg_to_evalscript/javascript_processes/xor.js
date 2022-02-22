c7844ab939355af1bdd87ff9e67f3679714805ff
Zan Pecovnik <zan.pecovnik@sinergise.com>
2022-02-22 16:41:09 +0100

    Add xor process

---
+++ src/pg_to_evalscript/javascript_processes/xor.js
@@ -0,0 +1,29 @@
+function xor(arguments) {
+  const { x, y } = arguments;
+
+  if (x === undefined) {
+    throw new Error("Mandatory argument `x` is not defined.");
+  }
+
+  if (y === undefined) {
+    throw new Error("Mandatory argument `y` is not defined.");
+  }
+
+  if (typeof x !== "boolean" && x !== null) {
+    throw new Error("Argument `x` is not a boolean or null.");
+  }
+
+  if (typeof y !== "boolean" && y !== null) {
+    throw new Error("Argument `y` is not a boolean or null.");
+  }
+
+  if (x === null || y === null) {
+    return null;
+  }
+
+  if (x === !y) {
+    return true;
+  }
+
+  return false;
+}
