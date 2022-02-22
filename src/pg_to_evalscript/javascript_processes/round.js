e5a7cd2bd81b60146267e8debac60d45663e9cbf
Zan Pecovnik <zan.pecovnik@sinergise.com>
2022-02-22 10:30:48 +0100

    Fix round process and add test cases

--- src/pg_to_evalscript/javascript_processes/round.js
+++ src/pg_to_evalscript/javascript_processes/round.js
@@ -17,10 +17,25 @@ function round(arguments) {
     throw new Error("Argument `p` is not a number.");
   }
 
+  if (!Number.isInteger(p)) {
+    throw new Error("Argument `p` is not an integer.");
+  }
+
   if (p < 0) {
     return (
       Math.round(x / Math.pow(10, Math.abs(p))) * Math.pow(10, Math.abs(p))
     );
   }
+
+  if (p === 0) {
+    if (Math.abs(x) - Math.trunc(Math.abs(x)) === 0.5) {
+      if (Math.floor(x) % 2 === 0) {
+        return Math.floor(x);
+      } else {
+        return Math.ceil(x);
+      }
+    }
+  }
+
   return Number(x.toFixed(p));
 }
--- tests/unit_tests/test_round.py
+++ tests/unit_tests/test_round.py
@@ -14,6 +14,7 @@ def round_process_code():
     "example_input,expected_output",
     [
         ({"x": 0}, 0),
+        ({"x": -2.5}, -2),
         ({"x": 3.5}, 4),
         ({"x": 0.4}, 0),
         ({"x": -0.4}, 0),
@@ -22,7 +23,10 @@ def round_process_code():
         ({"x": -0.00001}, 0),
         ({"x": 0.99999}, 1),
         ({"x": -0.99999}, -1),
+        ({"x":  2.6}, 3),
+        ({"x": 1234.5, "p": -2}, 1200),
         ({"x": 1.5, "p": 0}, 2),
+        ({"x": -2.5, "p": 0}, -2),
         ({"x": 3.2421, "p": 1}, 3.2),
         ({"x": 3.2421, "p": 3}, 3.242),
         ({"x": 3.2425, "p": 3}, 3.243),
@@ -30,6 +34,8 @@ def round_process_code():
         ({"x": 1234, "p": -1}, 1230),
         ({"x": 12345, "p": -1}, 12350),
         ({"x": 12345, "p": -3}, 12000),
+        ({"x": 3.56, "p": 1}, 3.6),
+        ({"x": -0.4444444, "p": 2}, -0.44),
         ({"x": None, "p": 3}, None),
         ({"x": None}, None),
     ],
@@ -48,6 +54,7 @@ def test_round(round_process_code, example_input, expected_output):
         ({"y": 0.5}, True, "Mandatory argument `x` is not defined."),
         ({"x": "0.5"}, True, "Argument `x` is not a number."),
         ({"x": 0.2, "p": "2"}, True, "Argument `p` is not a number."),
+        ({"x": 0.3, "p": 1.3}, True, "Argument `p` is not an integer.")
     ],
 )
 def test_round_exceptions(round_process_code, example_input, raises_exception, error_message):
