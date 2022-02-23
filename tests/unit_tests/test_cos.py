6c39e05a5ff6cde366d66ad164b7b3a51c55b012
Ziga Cernigoj <ziga.cernigoj@sinergise.com>
2022-02-23 15:31:58 +0100

    test_cos: call pytest.approx only 1 time

--- tests/unit_tests/test_cos.py
+++ tests/unit_tests/test_cos.py
@@ -15,21 +15,21 @@ def cos_process_code():
     "example_input,expected_output",
     [
         ({"x": 0}, 1),  # cos(0°)
-        ({"x": math.pi / 6}, pytest.approx(math.sqrt(3) / 2)),  # cos(30°)
-        ({"x": math.pi / 4}, pytest.approx(math.sqrt(2) / 2)),  # cos(45°)
-        ({"x": math.pi / 3}, pytest.approx(0.5)),  # cos(60°)
-        ({"x": math.pi / 2}, pytest.approx(0)),  # cos(90°)
+        ({"x": math.pi / 6}, math.sqrt(3) / 2),  # cos(30°)
+        ({"x": math.pi / 4}, math.sqrt(2) / 2),  # cos(45°)
+        ({"x": math.pi / 3}, 0.5),  # cos(60°)
+        ({"x": math.pi / 2}, 0),  # cos(90°)
         ({"x": math.pi}, -1),  # cos(180°)
-        ({"x": 3 * math.pi / 2}, pytest.approx(0)),  # cos(270°)
+        ({"x": 3 * math.pi / 2}, 0),  # cos(270°)
         ({"x": 2 * math.pi}, 1),  # cos(360°)
-        ({"x": -math.pi / 2}, pytest.approx(0)),  # cos(-90°)
+        ({"x": -math.pi / 2}, 0),  # cos(-90°)
         ({"x": None}, None),
     ],
 )
 def test_cos(cos_process_code, example_input, expected_output):
     output = run_process(cos_process_code, "cos", example_input)
     output = json.loads(output)
-    assert output == expected_output
+    assert pytest.approx(output) == expected_output
 
 
 @pytest.mark.parametrize(
