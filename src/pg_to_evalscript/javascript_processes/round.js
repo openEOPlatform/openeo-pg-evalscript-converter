c6e73f0fe4d89f6626789e992d091fd26253fb7b
Zan Pecovnik <zan.pecovnik@sinergise.com>
2022-02-24 10:36:36 +0100

    change var with const

--- src/pg_to_evalscript/javascript_processes/round.js
+++ src/pg_to_evalscript/javascript_processes/round.js
@@ -20,11 +20,12 @@ function round(arguments) {
   /**
    * implemented from https://stackoverflow.com/questions/3108986/gaussian-bankers-rounding-in-javascript/3109234#3109234
    */
-  var m = Math.pow(10, p);
-  var n = +(p ? x * m : x).toFixed(8); // Avoid rounding errors
-  var i = Math.floor(n),
+  const m = Math.pow(10, p);
+  const n = +(p ? x * m : x).toFixed(8); // Avoid rounding errors
+  const i = Math.floor(n),
     f = n - i;
-  var e = 1e-8; // Allow for rounding errors in f
-  var r = f > 0.5 - e && f < 0.5 + e ? (i % 2 == 0 ? i : i + 1) : Math.round(n);
+  const e = 1e-8; // Allow for rounding errors in f
+  const r =
+    f > 0.5 - e && f < 0.5 + e ? (i % 2 == 0 ? i : i + 1) : Math.round(n);
   return p ? r / m : r;
 }
