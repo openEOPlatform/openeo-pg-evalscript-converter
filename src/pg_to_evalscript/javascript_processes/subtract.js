72059ba92a7b99ed8051dcd3db26cb6c165df15b
Zan Pecovnik <zan.pecovnik@sinergise.com>
2022-02-25 14:57:50 +0100

    Add additional exception checks and test cases for subtract process

--- src/pg_to_evalscript/javascript_processes/subtract.js
+++ src/pg_to_evalscript/javascript_processes/subtract.js
@@ -1,8 +1,24 @@
 function subtract(arguments) {
   const { x, y } = arguments;
 
+  if (x === undefined) {
+    throw new Error("Mandatory argument `x` is not defined.");
+  }
+
+  if (y === undefined) {
+    throw new Error("Mandatory argument `y` is not defined.");
+  }
+
   if (x === null || y === null) {
     return null;
+  }
+
+  if (typeof x !== "number") {
+    throw new Error("Argument `x` is not a number.");
+  }
+
+  if (typeof y !== "number") {
+    throw new Error("Argument `y` is not a number.");
   }
 
   return x - y;
--- tests/unit_tests/test_subtract.py
+++ tests/unit_tests/test_subtract.py
@@ -16,6 +16,7 @@ def subtract_process_code():
         ({"x": 1, "y": 2}, -1),
         ({"x": None, "y": 3}, None),
         ({"x": 1, "y": None}, None),
+        ({"x": None, "y": None}, None),
         ({"x": -2, "y": 26}, -28),
         ({"x": -3, "y": -8}, 5),
         ({"x": 0, "y": -6}, 6),
@@ -29,3 +30,24 @@ def test_subtract(subtract_process_code, example_input, expected_output):
     output = run_process(subtract_process_code, "subtract", example_input)
     output = json.loads(output)
     assert pytest.approx(output) == expected_output
+
+
+@pytest.mark.parametrize(
+    "example_input,raises_exception,error_message",
+    [
+        ({"x": 1, "y": 2}, False, None),
+        ({}, True, "Mandatory argument `x` is not defined."),
+        ({"y": 2}, True, "Mandatory argument `x` is not defined."),
+        ({"x": 1}, True, "Mandatory argument `y` is not defined."),
+        ({"x": False, "y": 2}, True, "Argument `x` is not a number."),
+        ({"x": 1, "y": False}, True, "Argument `y` is not a number."),
+    ],
+)
+def test_subtract_exceptions(subtract_process_code, example_input, raises_exception, error_message):
+    if raises_exception:
+        with pytest.raises(Exception) as exc:
+            run_process(subtract_process_code, "subtract", example_input)
+        assert error_message in str(exc.value)
+
+    else:
+        run_process(subtract_process_code, "subtract", example_input)
