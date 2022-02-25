7009f2f6d632491555e6a783e01b6dc012284941
Zan Pecovnik <zan.pecovnik@sinergise.com>
2022-02-25 14:28:06 +0100

    Refactor code so it iterates only once through array. Add additional exception checks and test cases for max process.

--- src/pg_to_evalscript/javascript_processes/max.js
+++ src/pg_to_evalscript/javascript_processes/max.js
@@ -1,11 +1,42 @@
 function max(arguments) {
   const { data, ignore_nodata = true } = arguments;
-  if (
-    (!ignore_nodata && data.includes(null)) ||
-    data.length === 0 ||
-    data.every((x) => x === null)
-  ) {
-    return null;
+
+  if (data === undefined) {
+    throw new Error("Mandatory argument `data` is not defined.");
   }
-  return Math.max(...data.filter((i) => i !== null));
+
+  if (!Array.isArray(data)) {
+    throw new Error("Argument `data` is not an array.");
+  }
+
+  if (typeof ignore_nodata !== "boolean") {
+    throw new Error("Argument `ignore_nodata` is not a boolean.");
+  }
+
+  let maxVal = null;
+
+  for (let x of data) {
+    if (typeof x !== "number" && x !== null) {
+      throw new Error("Element in argument `data` is not a number or null.");
+    }
+
+    if (x === null) {
+      if (ignore_nodata) {
+        continue;
+      } else {
+        return null;
+      }
+    }
+
+    if (maxVal === null) {
+      maxVal = x;
+      continue;
+    }
+
+    if (x > maxVal) {
+      maxVal = x;
+    }
+  }
+
+  return maxVal;
 }
