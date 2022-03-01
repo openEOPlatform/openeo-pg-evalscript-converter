3ecc3acf329bbb581a2b34a0e9b58f5b5251ff5e
Zan Pecovnik <zan.pecovnik@sinergise.com>
2022-03-01 14:32:18 +0100

    Add variance process and test cases

---
+++ src/pg_to_evalscript/javascript_processes/variance.js
@@ -0,0 +1,50 @@
+function variance(arguments) {
+  const { data, ignore_nodata = true } = arguments;
+
+  if (data === undefined) {
+    throw new Error("Mandatory argument `data` is not defined.");
+  }
+
+  if (!Array.isArray(data)) {
+    throw new Error("Argument `data` is not an array.");
+  }
+
+  if (typeof ignore_nodata !== "boolean") {
+    throw new Error("Argument `ignore_nodata` is not a boolean.");
+  }
+
+  let sum = 0;
+  let count = 0;
+  for (let x of data) {
+    if (typeof x !== "number" && x !== null) {
+      throw new Error("Value in argument `data` is not a number or null.");
+    }
+
+    if (x === null) {
+      if (!ignore_nodata) {
+        return null;
+      } else {
+        continue;
+      }
+    }
+
+    sum += x;
+    count++;
+  }
+
+  if (count === 0) {
+    return null;
+  }
+
+  const mean = sum / count;
+  let sumOfSquares = 0;
+  for (let x of data) {
+    if (ignore_nodata && x === null) {
+      continue;
+    }
+
+    sumOfSquares += Math.pow(x - mean, 2);
+  }
+
+  return sumOfSquares / (count - 1);
+}
---
+++ tests/unit_tests/test_variance.py
@@ -0,0 +1,51 @@
+import json
+
+import pytest
+
+from tests.utils import load_process_code, run_process
+
+
+@pytest.fixture
+def variance_process_code():
+    return load_process_code("variance")
+
+
+@pytest.mark.parametrize(
+    "example_input,expected_output",
+    [
+        ({"data": [-1, 1, 3, None]}, 4),
+        ({"data": [-1, 1, 3, None], "ignore_nodata": False}, None),
+        ({"data": []}, None),
+        ({"data": [], "ignore_nodata": False}, None),
+        ({"data": [0, 1, 1, 2, 2, 3, 4, 5, 6, 6]}, 4.6666667),
+        ({"data": [0, 1, 1, None, 2, 2, 3, 4, 5, 6, 6]}, 4.6666667),
+        ({"data": [0, 1, 1, 2, 2, 3, 4, 5, None, 6, 6], "ignore_nodata": False}, None),
+        ({"data": [None]}, None),
+        ({"data": [None, None, None]}, None),
+        ({"data": [None, None, None], "ignore_nodata": False}, None),
+    ],
+)
+def test_variance(variance_process_code, example_input, expected_output):
+    output = run_process(variance_process_code, "variance", example_input)
+    output = json.loads(output)
+    assert pytest.approx(output) == expected_output
+
+
+@pytest.mark.parametrize(
+    "example_input,raises_exception,error_message",
+    [
+        ({"data": [-1, 1, 3, None]}, False, None),
+        ({}, True, "Mandatory argument `data` is not defined."),
+        ({"data": 2}, True, "Argument `data` is not an array."),
+        ({"data": [1, 2, 3], "ignore_nodata": 12}, True, "Argument `ignore_nodata` is not a boolean."),
+        ({"data": [1, 2, 3, 4, "5"]}, True, "Value in argument `data` is not a number or null."),
+    ],
+)
+def test_variance_exceptions(variance_process_code, example_input, raises_exception, error_message):
+    if raises_exception:
+        with pytest.raises(Exception) as exc:
+            run_process(variance_process_code, "variance", example_input)
+        assert error_message in str(exc.value)
+
+    else:
+        run_process(variance_process_code, "variance", example_input)
