f685320f64ff3698e65cbae576133dcabc06a040
Zan Pecovnik <zan.pecovnik@sinergise.com>
2022-03-01 16:28:36 +0100

    Add dimension_labels process and test cases

---
+++ tests/unit_tests/test_dimension_labels.py
@@ -0,0 +1,132 @@
+import json
+
+import pytest
+
+from tests.utils import load_process_code, load_datacube_code, run_process_with_additional_js_code
+
+
+@pytest.fixture
+def dimension_labels_process_code():
+    return load_process_code("dimension_labels")
+
+
+@pytest.mark.parametrize(
+    "example_input,expected_output",
+    [
+        (
+            {"data": {"B01": [1, 2, 3], "B02": [4, 5, 6], "B03": [7, 8, 9]}, "dimension": "bands_name"},
+            ["B01", "B02", "B03"],
+        ),
+        ({"data": {"B01": [1, 2, 3], "B02": [4, 5, 6], "B03": [7, 8, 9]}, "dimension": "temporal_name"}, []),
+        (
+            {
+                "data": {"B01": [1, 2, 3], "B02": [4, 5, 6], "B03": [7, 8, 9]},
+                "dimension": "temporal_name",
+                "additional_js_code_specific_to_case": "cube.getDimensionByName('temporal_name').labels = ['test1', 'test2', 'test3'];",
+            },
+            ["test1", "test2", "test3"],
+        ),
+    ],
+)
+def test_dimension_labels(dimension_labels_process_code, example_input, expected_output):
+    additional_js_code_to_run = (
+        load_datacube_code()
+        + f"const cube = new DataCube({example_input['data']}, 'bands_name', 'temporal_name', true);"
+        + f"{example_input['additional_js_code_specific_to_case'] if 'additional_js_code_specific_to_case' in example_input else ''}"
+    )
+    output = run_process_with_additional_js_code(
+        dimension_labels_process_code,
+        "dimension_labels",
+        example_input,
+        additional_js_code_to_run,
+        additional_params_in_string="'data': cube",
+    )
+    output = json.loads(output)
+    assert output == expected_output
+
+
+@pytest.mark.parametrize(
+    "example_input,raises_exception,error_message",
+    [
+        (
+            {"data": {"B01": [1, 2, 3], "B02": [4, 5, 6], "B03": [7, 8, 9]}, "dimension": "bands_name"},
+            False,
+            None,
+        ),
+        (
+            {
+                "data": {"B01": [1, 2, 3], "B02": [4, 5, 6], "B03": [7, 8, 9]},
+                "dimension": "bands_name",
+                "additional_js_code_specific_to_case": "cube = undefined;",
+            },
+            True,
+            "Mandatory argument `data` is not defined.",
+        ),
+        (
+            {
+                "data": {"B01": [1, 2, 3], "B02": [4, 5, 6], "B03": [7, 8, 9]},
+            },
+            True,
+            "Mandatory argument `dimension` is not defined.",
+        ),
+        (
+            {
+                "data": {"B01": [1, 2, 3], "B02": [4, 5, 6], "B03": [7, 8, 9]},
+                "dimension": 123,
+            },
+            True,
+            "Argument `dimension` is not a string.",
+        ),
+        (
+            {
+                "data": {"B01": [1, 2, 3], "B02": [4, 5, 6], "B03": [7, 8, 9]},
+                "dimension": "fake_name",
+            },
+            True,
+            "A dimension with the specified name does not exist.",
+        ),
+        (
+            {
+                "data": {"B01": [1, 2, 3], "B02": [4, 5, 6], "B03": [7, 8, 9]},
+                "dimension": "bands_name",
+                "additional_js_code_specific_to_case": "cube.getDimensionByName('bands_name').labels = undefined;",
+            },
+            True,
+            "Dimension is missing attribute labels.",
+        ),
+        (
+            {
+                "data": {"B01": [1, 2, 3], "B02": [4, 5, 6], "B03": [7, 8, 9]},
+                "dimension": "bands_name",
+                "additional_js_code_specific_to_case": "cube.getDimensionByName('bands_name').labels = '[]';",
+            },
+            True,
+            "Dimension labels is not an array.",
+        ),
+    ],
+)
+def test_dimension_labels_exceptions(dimension_labels_process_code, example_input, raises_exception, error_message):
+    additional_js_code_to_run = (
+        load_datacube_code()
+        + f"const cube = new DataCube({example_input['data']}, 'bands_name', 'temporal_name', true);"
+        + f"{example_input['additional_js_code_specific_to_case'] if 'additional_js_code_specific_to_case' in example_input else ''}"
+    )
+    if raises_exception:
+        with pytest.raises(Exception) as exc:
+            run_process_with_additional_js_code(
+                dimension_labels_process_code,
+                "dimension_labels",
+                example_input,
+                additional_js_code_to_run,
+                additional_params_in_string="'data': cube",
+            )
+        assert error_message in str(exc.value)
+
+    else:
+        run_process_with_additional_js_code(
+            dimension_labels_process_code,
+            "dimension_labels",
+            example_input,
+            additional_js_code_to_run,
+            additional_params_in_string="'data': cube",
+        )
--- tests/utils.py
+++ tests/utils.py
@@ -27,6 +27,20 @@ def run_process(process_code, process_name, example_input):
     )
 
 
+def run_process_with_additional_js_code(
+    process_code,
+    process_name,
+    example_input,
+    additional_js_code_to_run,
+    additional_params_in_string,
+):
+    return run_javacript(
+        process_code
+        + additional_js_code_to_run
+        + f"process.stdout.write(JSON.stringify({process_name}({{...{json.dumps(example_input)}, {additional_params_in_string}}})));"
+    )
+
+
 def get_evalscript_input_object(evalscript):
     return json.loads(run_javacript(evalscript + f"\nprocess.stdout.write(JSON.stringify(setup()))"))
 
@@ -38,6 +52,13 @@ def run_javacript(javascript_code):
 def load_script(source_file_folder, source_file_name):
     script_dir = os.path.dirname(__file__)
     abs_file_path = os.path.join(script_dir, f"{source_file_folder}/{source_file_name}.js")
+    with open(abs_file_path) as f:
+        return f.read()
+
+
+def load_datacube_code():
+    script_dir = os.path.dirname(__file__)
+    abs_file_path = os.path.join(script_dir, f"../src/pg_to_evalscript/javascript_datacube/DataCube.js")
     with open(abs_file_path) as f:
         return f.read()
 
