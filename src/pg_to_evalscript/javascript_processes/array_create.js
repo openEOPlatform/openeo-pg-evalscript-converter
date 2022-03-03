e46b96d64d4837c847b7d56d9832d61356b6a447
Zan Pecovnik <zan.pecovnik@sinergise.com>
2022-03-03 13:43:43 +0100

    Add array_create process and test cases

---
+++ src/pg_to_evalscript/javascript_processes/array_create.js
@@ -0,0 +1,23 @@
+function array_create(arguments) {
+  const { data = [], repeat = 1 } = arguments;
+
+  if (!Array.isArray(data)) {
+    throw new Error("Argument `data` is not an array.");
+  }
+
+  if (!Number.isInteger(repeat)) {
+    throw new Error("Argument `repeat` is not an integer.");
+  }
+
+  if (repeat < 1) {
+    throw new Error("Argument `repeat` has a minimum value of 1.");
+  }
+
+  let newData = [];
+
+  for (let i = 0; i < repeat; i++) {
+    newData = [...newData, ...data];
+  }
+
+  return newData;
+}
