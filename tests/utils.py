787b50541c2630456b1becebc4ee5d4bf7e8302d
Zan Pecovnik <zan.pecovnik@sinergise.com>
2022-03-04 11:38:22 +0100

    Remove not needed code

--- tests/utils.py
+++ tests/utils.py
@@ -55,13 +55,6 @@ def load_script(source_file_folder, source_file_name):
         return f.read()
 
 
-def load_datacube_code():
-    script_dir = os.path.dirname(__file__)
-    abs_file_path = os.path.join(script_dir, f"../src/pg_to_evalscript/javascript_datacube/DataCube.js")
-    with open(abs_file_path) as f:
-        return f.read()
-        
-
 def load_process_code(process_id):
     source_files = [
         load_script("../src/pg_to_evalscript/javascript_common/", "common"),
