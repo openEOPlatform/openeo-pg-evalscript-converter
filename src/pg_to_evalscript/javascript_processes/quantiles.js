c1944b39084e8a3071eaa9e2354b7a1fafceded3
Ziga Cernigoj <ziga.cernigoj@sinergise.com>
2022-03-04 11:43:58 +0100

    add quantiles process and tests

---
+++ src/pg_to_evalscript/javascript_processes/quantiles.js
@@ -0,0 +1,99 @@
+class QuantilesParameterMissing extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "QuantilesParameterMissing";
+  }
+}
+
+class QuantilesParameterConflict extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "QuantilesParameterConflict";
+  }
+}
+
+function quantiles(arguments) {
+  const { data, probabilities, q, ignore_nodata = true } = arguments;
+
+  if (data === undefined || data === null) {
+    throw new Error("Mandatory argument `data` is either null or not defined");
+  }
+
+  if (!Array.isArray(data)) {
+    throw new Error("Argument `data` is not an array.");
+  }
+
+  if (typeof ignore_nodata !== "boolean") {
+    throw new Error("Argument `ignore_nodata` is not a boolean.");
+  }
+
+  if (probabilities === undefined && q === undefined) {
+    throw new QuantilesParameterMissing("The process `quantiles` requires either the `probabilities` or `q` parameter to be set.");
+  }
+
+  if (probabilities !== undefined && q !== undefined) {
+    throw new QuantilesParameterConflict("The process `quantiles` only allows that either the `probabilities` or the `q` parameter is set.");
+  }
+
+  let probs = [];
+
+  if (!!q && probabilities === undefined) {
+    if (!Number.isInteger(q)) {
+      throw new Error("Argument `q` is not an integer.");
+    }
+
+    if (q < 2) {
+      throw new Error("Argument `q` must be greater or equal to 2.");
+    }
+
+    for (let i = 0; i < 1; i += 1 / q) {
+      probs.push(i);
+    }
+
+    probs = probs.slice(1);
+  }
+
+  if (!!probabilities && q === undefined) {
+    if (!Array.isArray(probabilities)) {
+      throw new Error("Argument `probabilities` is not an array.");
+    }
+
+    probs = probabilities;
+  }
+
+  let newData = [...data].sort((a, b) => a - b);
+
+  if (!ignore_nodata && newData.includes(null)) {
+    return Array.from(probs).fill(null);
+  }
+
+  newData = newData.filter(el => el !== null);
+
+  if (newData.length === 0) {
+    return Array.from(probs).fill(null);
+  }
+
+  let quantiles = [];
+
+  for (const el of probs) {
+    if (!!probabilities && q === undefined) {
+      if (typeof el !== 'number') {
+        throw new Error("Element in argument `probabilities` is not a number.");
+      }
+      if (el < 0 || el > 1) {
+        throw new Error("Elements in argument `probabilities` must be between 0 and 1 (both inclusive).");
+      }
+    }
+
+    const pos = (newData.length - 1) * el;
+    const base = Math.floor(pos);
+    const rest = pos - base;
+    if (newData[base + 1] !== undefined) {
+      quantiles.push(newData[base] + rest * (newData[base + 1] - newData[base]));
+    } else {
+      quantiles.push(newData[base]);
+    }
+  }
+
+  return quantiles;
+}
