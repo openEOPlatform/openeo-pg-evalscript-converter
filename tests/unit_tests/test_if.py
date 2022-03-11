9e0a65dd11b30575821b1fcb022801b4e36389be
Damjan Gostenčnik <damjan.gostencnik@gmail.com>
2022-03-11 15:16:17 +0100

    use use validateParameter in if process

--- tests/unit_tests/test_if.py
+++ tests/unit_tests/test_if.py
@@ -2,7 +2,7 @@ import json
 
 import pytest
 
-from tests.utils import load_process_code, run_process
+from tests.utils import load_process_code, run_process, run_input_validation
 
 
 @pytest.fixture
@@ -15,11 +15,11 @@ def if_process_code():
     [
         ({"value": True, "accept": "A", "reject": "B"}, "A"),
         ({"value": False, "accept": "A", "reject": "B"}, "B"),
-        ({"value": 3**2 == 9, "accept": "equals", "reject": [1, 2, 3]}, "equals"),
+        ({"value": 3 ** 2 == 9, "accept": "equals", "reject": [1, 2, 3]}, "equals"),
         ({"value": None, "accept": "ABC", "reject": [1, 2, 3]}, [1, 2, 3]),
         ({"value": None, "accept": 1}, None),
         ({"value": False, "accept": 123}, None),
-        ({"value": True, "accept": 4**2}, 16),
+        ({"value": True, "accept": 4 ** 2}, 16),
     ],
 )
 def test_if(if_process_code, example_input, expected_output):
@@ -32,16 +32,10 @@ def test_if(if_process_code, example_input, expected_output):
     "example_input,raises_exception,error_message",
     [
         ({"value": True, "accept": "A", "reject": "B"}, False, None),
-        ({"accept": "A", "reject": "B"}, True, "Mandatory argument `value` is not defined."),
-        ({"value": True, "reject": "B"}, True, "Mandatory argument `accept` is not defined."),
-        ({"value": "True", "accept": "A", "reject": "B"}, True, "Argument `value` is not a boolean or null."),
+        ({"accept": "A", "reject": "B"}, True, "MISSING_PARAMETER"),
+        ({"value": True, "reject": "B"}, True, "MISSING_PARAMETER"),
+        ({"value": "True", "accept": "A", "reject": "B"}, True, "NOT_BOOLEAN"),
     ],
 )
 def test_if_exceptions(if_process_code, example_input, raises_exception, error_message):
-    if raises_exception:
-        with pytest.raises(Exception) as exc:
-            run_process(if_process_code, "_if", example_input)
-        assert error_message in str(exc.value)
-
-    else:
-        run_process(if_process_code, "_if", example_input)
+    run_input_validation(if_process_code, "_if", example_input, raises_exception, error_message)
