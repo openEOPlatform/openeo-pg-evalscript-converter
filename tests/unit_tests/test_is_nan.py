053c094005b0d46ef8bd6b708656774f6995a88b
Damjan Gostenčnik <damjan.gostencnik@gmail.com>
2022-03-11 15:25:12 +0100

    use use validateParameter in is_nan process

--- tests/unit_tests/test_is_nan.py
+++ tests/unit_tests/test_is_nan.py
@@ -2,7 +2,7 @@ import json
 
 import pytest
 
-from tests.utils import load_process_code, run_process
+from tests.utils import load_process_code, run_process, run_input_validation
 
 
 @pytest.fixture
@@ -23,3 +23,14 @@ def test_is_nan(is_nan_code, example_input, expected_output):
     output = run_process(is_nan_code, "is_nan", example_input)
     output = json.loads(output)
     assert output == expected_output
+
+
+@pytest.mark.parametrize(
+    "example_input,raises_exception,error_name",
+    [
+        ({"x": 1}, False, None),
+        ({}, True, "MISSING_PARAMETER"),
+    ],
+)
+def test_input_validation(is_nan_code, example_input, raises_exception, error_name):
+    run_input_validation(is_nan_code, "is_nan", example_input, raises_exception, error_name)
