5de07ddc1b63bd88477d618de1cbd773e66c52fb
Damjan Gostenčnik <damjan.gostencnik@gmail.com>
2022-03-11 15:25:23 +0100

    use use validateParameter in is_nodata process

--- tests/unit_tests/test_is_nodata.py
+++ tests/unit_tests/test_is_nodata.py
@@ -2,7 +2,7 @@ import json
 
 import pytest
 
-from tests.utils import load_process_code, run_process
+from tests.utils import load_process_code, run_process, run_input_validation
 
 
 @pytest.fixture
@@ -25,3 +25,14 @@ def test_is_nodata(is_nodata_code, example_input, expected_output):
     output = run_process(is_nodata_code, "is_nodata", example_input)
     output = json.loads(output)
     assert output == expected_output
+
+
+@pytest.mark.parametrize(
+    "example_input,raises_exception,error_name",
+    [
+        ({"x": 1}, False, None),
+        ({}, True, "MISSING_PARAMETER"),
+    ],
+)
+def test_input_validation(is_nodata_code, example_input, raises_exception, error_name):
+    run_input_validation(is_nodata_code, "is_nodata", example_input, raises_exception, error_name)
