1684269037677ca13d9c9e2d9455783a3f94d53e
Damjan Gostenčnik <damjan.gostencnik@gmail.com>
2022-03-11 15:25:32 +0100

    use use validateParameter in is_valid process

--- tests/unit_tests/test_is_valid.py
+++ tests/unit_tests/test_is_valid.py
@@ -2,7 +2,7 @@ import json
 
 import pytest
 
-from tests.utils import load_process_code, run_process
+from tests.utils import load_process_code, run_process, run_input_validation
 
 
 @pytest.fixture
@@ -26,3 +26,14 @@ def test_is_valid(is_valid_code, example_input, expected_output):
     output = run_process(is_valid_code, "is_valid", example_input)
     output = json.loads(output)
     assert output == expected_output
+
+
+@pytest.mark.parametrize(
+    "example_input,raises_exception,error_name",
+    [
+        ({"x": 1}, False, None),
+        ({}, True, "MISSING_PARAMETER"),
+    ],
+)
+def test_input_validation(is_valid_code, example_input, raises_exception, error_name):
+    run_input_validation(is_valid_code, "is_valid", example_input, raises_exception, error_name)
