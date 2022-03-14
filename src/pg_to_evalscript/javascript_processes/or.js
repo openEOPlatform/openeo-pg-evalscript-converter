6c0b62db759c76ab22c116b263f85efa4285d23c
Damjan Gostenčnik <damjan.gostencnik@gmail.com>
2022-03-14 13:55:05 +0100

    use use validateParameter in or process

--- src/pg_to_evalscript/javascript_processes/or.js
+++ src/pg_to_evalscript/javascript_processes/or.js
@@ -1,21 +1,21 @@
 function or(arguments) {
   const { x, y } = arguments;
 
-  if (x === undefined) {
-    throw new Error("Mandatory argument `x` is not defined.");
-  }
+  validateParameter({
+    processName: "or",
+    parameterName: "x",
+    value: x,
+    required: true,
+    allowedTypes: ["boolean"],
+  });
 
-  if (y === undefined) {
-    throw new Error("Mandatory argument `y` is not defined.");
-  }
-
-  if (typeof x !== "boolean" && x !== null) {
-    throw new Error("Argument `x` is not a boolean or null.");
-  }
-
-  if (typeof y !== "boolean" && y !== null) {
-    throw new Error("Argument `y` is not a boolean or null.");
-  }
+  validateParameter({
+    processName: "or",
+    parameterName: "y",
+    value: y,
+    required: true,
+    allowedTypes: ["boolean"],
+  });
 
   if (x === true || y === true) {
     return true;
