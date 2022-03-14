d64c6f25143276705b87797e5d34ec62cdb9fa49
Damjan Gostenčnik <damjan.gostencnik@gmail.com>
2022-03-14 15:18:38 +0100

    use use validateParameter in xor process

--- tests/unit_tests/test_xor.py
+++ tests/unit_tests/test_xor.py
@@ -2,7 +2,7 @@ import json
 
 import pytest
 
-from tests.utils import load_process_code, run_process
+from tests.utils import load_process_code, run_process, run_input_validation
 
 
 @pytest.fixture
@@ -34,19 +34,13 @@ def test_xor(xor_process_code, example_input, expected_output):
     "example_input,raises_exception,error_message",
     [
         ({"x": True, "y": True}, False, None),
-        ({}, True, "Mandatory argument `x` is not defined."),
-        ({"x": True}, True, "Mandatory argument `y` is not defined."),
-        ({"y": True}, True, "Mandatory argument `x` is not defined."),
-        ({"x": "True", "y": "True"}, True, "Argument `x` is not a boolean or null."),
-        ({"x": "True", "y": True}, True, "Argument `x` is not a boolean or null."),
-        ({"x": True, "y": "True"}, True, "Argument `y` is not a boolean or null."),
+        ({}, True, "MISSING_PARAMETER"),
+        ({"x": True}, True, "MISSING_PARAMETER"),
+        ({"y": True}, True, "MISSING_PARAMETER"),
+        ({"x": "True", "y": "True"}, True, "WRONG_TYPE"),
+        ({"x": "True", "y": True}, True, "WRONG_TYPE"),
+        ({"x": True, "y": "True"}, True, "WRONG_TYPE"),
     ],
 )
 def test_xor_exceptions(xor_process_code, example_input, raises_exception, error_message):
-    if raises_exception:
-        with pytest.raises(Exception) as exc:
-            run_process(xor_process_code, "xor", example_input)
-        assert error_message in str(exc.value)
-
-    else:
-        run_process(xor_process_code, "xor", example_input)
+    run_input_validation(xor_process_code, "xor", example_input, raises_exception, error_message)
