9e9e4a4470896f8201b9734e66d7d4b274012432
Zan Pecovnik <zan.pecovnik@sinergise.com>
2022-03-17 09:56:13 +0100

    Check input data parameter

--- src/pg_to_evalscript/node.py
+++ src/pg_to_evalscript/node.py
@@ -284,6 +284,16 @@ function array_apply(arguments) {{
     }}
 
     const {{data, context = null}} = arguments;
+
+    validateParameter({{
+        processName: "array_apply",
+        parameterName: "data",
+        value: data,
+        required: true,
+        nullable: false,
+        array: true
+    }});
+
     let newData = [];
     for (let i = 0; i < data.length; i++) {{
         newData[i] = process({{
