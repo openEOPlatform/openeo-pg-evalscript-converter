e91c0824236edf9a5fb1143c3aeafa22c1dcfe8b
Zan Pecovnik <zan.pecovnik@sinergise.com>
2022-03-17 10:54:23 +0100

    Replace finding dimension data with getDataShape

--- src/pg_to_evalscript/javascript_processes/drop_dimension.js
+++ src/pg_to_evalscript/javascript_processes/drop_dimension.js
@@ -23,16 +23,9 @@ function drop_dimension(arguments) {
     throw new Error("A dimension with the specified name does not exist.");
   }
 
-  function getDataForDimension(data, currLevel, dimLevel) {
-    if (currLevel === dimLevel) {
-      return data;
-    }
-    return getDataForDimension(data[0], currLevel + 1, dimLevel);
-  }
-
-  const dimLevel = data.dimensions.findIndex((d) => d.name === name);
-  const dimData = getDataForDimension(data.data, 0, dimLevel);
-  if (dimData.length > 1) {
+  const dimIndex = data.dimensions.findIndex((d) => d.name === name);
+  const dimData = data.getDataShape();
+  if (dimData[dimIndex] > 1) {
     throw new Error(
       "The number of dimension labels exceeds one, which requires a reducer."
     );
