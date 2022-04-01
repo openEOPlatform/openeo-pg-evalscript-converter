abe5ff16431be0b790f05a3e063f73c0f8324e8d
Ziga Cernigoj <ziga.cernigoj@sinergise.com>
2022-04-01 12:55:50 +0200

    mask: add test for empty data and test

--- tests/unit_tests/test_mask.py
+++ tests/unit_tests/test_mask.py
@@ -73,6 +73,46 @@ def mask_process_code():
                 "data": {'data': [1, 'aaa', 3], 'offset': 0, 'shape': [3, 1], 'stride': [1, 1]},
             },
         ),
+
+        # multiple labels for bands
+        (
+            {
+                "data": [{"B01": 1, "B02": 1}, {"B01": 2, "B02": 2}, {"B01": 3, "B02": 3}],
+                "mask": [{"B01": 0, "B02": 0}, {"B01": 1, "B02": 0}, {"B01": 1, "B02": 1}],
+            },
+            {
+                "BANDS": "bands",
+                "OTHER": "other",
+                "TEMPORAL": "temporal",
+                "bands_dimension_name": "bands_name",
+                "temporal_dimension_name": "temporal_name",
+                "dimensions": [
+                    {"labels": [], "name": "temporal_name", "type": "temporal"},
+                    {"labels": ["B01", "B02"], "name": "bands_name", "type": "bands"},
+                ],
+                "data": {'data': [1, 1, None, 2, None, None], 'offset': 0, 'shape': [3, 2], 'stride': [2, 1]},
+            },
+        ),
+
+        # no data, no mask
+        (
+            {
+                "data": [],
+                "mask": [],
+            },
+            {
+                "BANDS": "bands",
+                "OTHER": "other",
+                "TEMPORAL": "temporal",
+                "bands_dimension_name": "bands_name",
+                "temporal_dimension_name": "temporal_name",
+                "dimensions": [
+                    {"labels": [], "name": "temporal_name", "type": "temporal"},
+                    {"labels": [], "name": "bands_name", "type": "bands"},
+                ],
+                "data": {'data': [], 'offset': 0, 'shape': [0], 'stride': [1]},
+            },
+        ),
     ],
 )
 def test_mask(mask_process_code, example_input, expected_output):
@@ -103,17 +143,23 @@ def test_mask(mask_process_code, example_input, expected_output):
     process_arguments = ( f"{{" + arguments + f"}}" )
 
 
-    # print("// ---------- additional js code -------------")
-    # print(additional_js_code_to_run)
-
-    # print("// ---------- mask process code -------------")
-    # print(mask_process_code)
-    
     # print("// ------------ vars_def ------------")
     # print(vars_definitions)
+
+#     print("// ---------- additional js code -------------")
+#     print(additional_js_code_to_run)
+
+#     print("// ---------- mask process code -------------")
+#     print(mask_process_code)
     
-    # print("// ------------ arg ------------")
-    # print(process_arguments)
+#     print("// ------------ arg ------------")
+#     print("const res = mask(" + process_arguments + ")")
+#     console_log = '''console.log('res', {
+#   res,
+#   resJSON: JSON.stringify(res),
+#   d: res.data
+# });'''
+#     print(console_log)
 
     output = run_process(
         mask_process_code + additional_js_code_to_run,
