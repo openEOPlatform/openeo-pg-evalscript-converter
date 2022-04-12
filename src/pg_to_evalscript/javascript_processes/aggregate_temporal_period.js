f590515bcae500ba4b1071d6419c77daaf6cbeff
Zan Pecovnik <zan.pecovnik@sinergise.com>
2022-04-12 10:25:45 +0200

    function for formatting new labels

--- src/pg_to_evalscript/javascript_processes/aggregate_temporal_period.js
+++ src/pg_to_evalscript/javascript_processes/aggregate_temporal_period.js
@@ -1,4 +1,84 @@
 function aggregate_temporal_period(arguments) {
+  const formatLabelByPeriod = (periodType, label) => {
+    const dayInYear = (date) => {
+      return (
+        (Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()) -
+          Date.UTC(date.getFullYear(), 0, 0)) /
+        24 /
+        60 /
+        60 /
+        1000
+      );
+    };
+
+    const dekadInYear = (date) => {
+      const monthCount = date.getMonth();
+      const dekadCount = Math.floor((date.getDate() - 1) / 10) + 1;
+      return monthCount * 3 + (dekadCount > 2 ? 3 : dekadCount);
+    };
+
+    const padWithZeros = (num, size) => {
+      num = num.toString();
+      while (num.length < size) {
+        num = "0" + num;
+      }
+      return num;
+    };
+
+    const d = new Date(parse_rfc3339(label).value);
+    switch (periodType) {
+      case "hour":
+        const hourCount = d.getHours();
+        const days = d.getDate();
+        const months = d.getMonth() + 1;
+        return `${d.getFullYear()}-${padWithZeros(months, 2)}-${padWithZeros(
+          days,
+          2
+        )}-${padWithZeros(hourCount, 2)}`;
+      case "day":
+        const dayCount = dayInYear(d);
+        return `${d.getFullYear()}-${padWithZeros(dayCount, 3)}`;
+      case "week":
+        const weekCount = Math.ceil(dayInYear(d) / 7);
+        return `${d.getFullYear()}-${padWithZeros(weekCount, 2)}`;
+      case "dekad":
+        const dekadCount = dekadInYear(d);
+        return `${d.getFullYear()}-${padWithZeros(dekadCount, 2)}`;
+      case "month":
+        const monthCount = d.getMonth() + 1;
+        return `${d.getFullYear()}-${padWithZeros(monthCount, 2)}`;
+      case "season":
+        const month = d.getMonth() + 1;
+        let seasonName = null;
+        if (month >= 3 && month <= 5) {
+          seasonName = "mam";
+        } else if (month >= 6 && month <= 8) {
+          seasonName = "jja";
+        } else if (month >= 9 && month <= 11) {
+          seasonName = "son";
+        } else {
+          seasonName = "djf";
+        }
+        return `${d.getFullYear()}-${seasonName}`;
+      case "tropical-season":
+        let tropicalSeasonName = null;
+        if (d.getMonth() + 1 >= 5 && d.getMonth() + 1 <= 10) {
+          tropicalSeasonName = "mjjaso";
+        } else {
+          tropicalSeasonName = "ndjfma";
+        }
+        return `${d.getFullYear()}-${tropicalSeasonName}`;
+      case "year":
+        return `${d.getFullYear()}`;
+      case "decade":
+        return `${d.getFullYear().toString().substring(0, 3)}0`;
+      case "decade-ad":
+        return `${d.getFullYear().toString().substring(0, 3)}1`;
+      default:
+        return `${d.getFullYear()}`;
+    }
+  };
+
   const { data, period, reducer, dimension = null, context = null } = arguments;
 
   validateParameter({
@@ -61,7 +141,6 @@ function aggregate_temporal_period(arguments) {
     });
   }
 
-  // 1 more exception to handle DistinctDimensionLabelsRequired
   // add code for aggregating
 
   return newData;
