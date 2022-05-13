a7705319ecf05415e6e5388f97897d0722e861e3
Ziga Cernigoj <ziga.cernigoj@sinergise.com>
2022-05-13 10:41:23 +0200

    mask process: fix order of dimensions in mask argument so it's the same as in data argument

--- src/pg_to_evalscript/javascript_processes/mask.js
+++ src/pg_to_evalscript/javascript_processes/mask.js
@@ -39,10 +39,83 @@ function mask(arguments) {
     }
   }
 
+  // NAČIN 1
+  // najprej obstoječe dimenzije v mask daj v "isti" vrstni red kot v data
+  // potem dodaj manjkajoče dimenzije 
+
+  // NAČIN 2 - uporabljen
+  // najprej dodaj manjkajoče dimenzije (na začetek)
+  // potem spremeni vrstni red mask, da bo enak kot v data
+
+  let maskWithMissingDims = mask.clone();
+  for (let dataDim of data.dimensions) {
+    if (!maskWithMissingDims.getDimensionByName(dataDim.name)) {
+      maskWithMissingDims.addDimension(dataDim.name, "label to be removed", dataDim.type);
+      maskWithMissingDims.setDimensionLabels(dataDim.name, []);
+    }
+  }
+
+  // use THIS to get the correct order of the dimensions
+  // then transpose the data matrix!!!
+
+  // let dimensionsOrder = [];
+  let dimsOrd = []
+  for (let dataDimIndex in data.dimensions) {
+    const dataDim = data.dimensions[parseInt(dataDimIndex)];
+    const maskDimIndex = maskWithMissingDims.dimensions.findIndex((d) => d.name === dataDim.name);
+
+    // add to mask
+
+    if (maskDimIndex === -1) {
+      throw new Error("AAAAAAAAAAAAAAAAAAAAAAAA" + JSON.stringify({
+        dataDimIndex,
+        maskDimIndex,
+        dataDim
+      }));
+    }
+
+    // dimensionsOrder.push({
+    //   wantedIndex: parseInt(dataDimIndex),
+    //   currentIndex: maskDimIndex
+    // });
+    dimsOrd.push(maskDimIndex);
+  }
+
+
+  // data, bands_dimension_name, temporal_dimension_name, fromSamples, bands_metadata, scenes
+  let correctedMaskWithDims = maskWithMissingDims.clone();
+
+  correctedMaskWithDims.data = correctedMaskWithDims.data.transpose(...dimsOrd);
+
+  // fix order of dimensions for the correctedMaskWithDims Datacube
+
+  const dimensionsOldOrder = correctedMaskWithDims.dimensions.slice();
+  correctedMaskWithDims.dimensions = dimsOrd.map(val => dimensionsOldOrder[val]);
+
+  // broadcast correctedMaskWithDims.data
+  // let newData2 = data.clone();
+  // const maskBroadcasted2 = broadcastNdarray(correctedMaskWithDims.data, newData2.data.shape);
+
+  // then it's ready for masking
+
+  // throw new Error(
+  //   "MISSING ___ " + 
+  //   JSON.stringify({
+  //     mask,
+  //     // data,
+  //     maskWithMissingDims,
+  //     correctedMaskWithDims,
+  //     maskBroadcasted2,
+  //     // dimensionsOrder,
+  //     // dimsOrd,
+  //   }) + 
+  //   " ___ MISSING"
+  // );
+
   let newData = data.clone();
   let newDataFlat = newData.flattenToArray();
 
-  const maskBroadcasted = broadcastNdarray(mask.data, newData.data.shape);
+  const maskBroadcasted = broadcastNdarray(correctedMaskWithDims.data, newData.data.shape);
   const maskBroadcastedFlat = flattenToNativeArray(maskBroadcasted);
 
   const replacement_val = replacement === undefined ? null : replacement;
@@ -55,6 +128,16 @@ function mask(arguments) {
       newDataFlat[i] = replacement_val;
     }
   }
+
+  // throw new Error(
+  //   "EEE ___ " +
+  //   JSON.stringify({
+  //     data,
+  //     mask,
+  //     newData,
+  //   }) +
+  //   " ___ EEE"
+  // );
 
   return newData;
 }
