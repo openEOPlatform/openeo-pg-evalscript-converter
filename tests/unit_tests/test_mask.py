bbeb635ac173078ea19df10ce9084c9d7f22c750
Ziga Cernigoj <ziga.cernigoj@sinergise.com>
2022-05-16 12:48:31 +0200

    test_mask.py: don't pass unneeded parameter to run_process()

--- tests/unit_tests/test_mask.py
+++ tests/unit_tests/test_mask.py
@@ -362,7 +362,7 @@ def test_mask_exceptions(
         + (additional_js_code_specific_to_case or "")
     )
 
-    arguments = f"'data': dataCube, 'mask': maskCube, 'scenes': {scenes_data}"
+    arguments = f"'data': dataCube, 'mask': maskCube"
 
     if "replacement" in example_input:
         arguments = arguments + f", 'replacement': {replacement_parameter}"
