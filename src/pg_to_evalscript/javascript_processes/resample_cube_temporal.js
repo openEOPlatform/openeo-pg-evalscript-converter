6f5d6896bf084c7d566a9e70672f06ec9f1f4b51
Ziga Cernigoj <ziga.cernigoj@sinergise.com>
2022-06-03 09:51:07 +0200

    resample_cube_temporal process: make data and target params not nullable

--- src/pg_to_evalscript/javascript_processes/resample_cube_temporal.js
+++ src/pg_to_evalscript/javascript_processes/resample_cube_temporal.js
@@ -6,6 +6,7 @@ function resample_cube_temporal(arguments) {
     parameterName: "data",
     value: data,
     required: true,
+    nullable: false,
   });
 
   validateParameter({
@@ -13,6 +14,7 @@ function resample_cube_temporal(arguments) {
     parameterName: "target",
     value: target,
     required: true,
+    nullable: false,
   });
 
   validateParameter({
