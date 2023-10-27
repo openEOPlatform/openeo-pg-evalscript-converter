613b831df558788d9067777970d67fe791bd734e
Ziga Cernigoj <ziga.cernigoj@sinergise.com>
2023-10-27 11:40:14 +0200

    expose implicitly_supported_processes from pg_to_evalscript/utils.py to avoid code duplication

--- src/pg_to_evalscript/utils.py
+++ src/pg_to_evalscript/utils.py
@@ -1,9 +1,9 @@
 import pkg_resources
 
+implicitly_supported_processes = ["load_collection", "save_result"]
 
 def list_supported_processes():
     process_definitions_directory = "javascript_processes"
-    implicitly_supported_processes = ["load_collection", "save_result"]
     process_definition_files = pkg_resources.resource_listdir("pg_to_evalscript", f"{process_definitions_directory}")
     supported_processes_with_files = [
         process_definition_file.replace(".js", "") for process_definition_file in process_definition_files
