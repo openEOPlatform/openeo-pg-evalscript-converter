e1fcb23477fe97e9e39793748b8a0bd98b7c2f44
Ziga Cernigoj <ziga.cernigoj@sinergise.com>
2023-10-27 11:35:15 +0200

    remove apply and reduce_dimension processes from implicitly_supported_processes list as they have js files

--- src/pg_to_evalscript/utils.py
+++ src/pg_to_evalscript/utils.py
@@ -3,7 +3,7 @@ import pkg_resources
 
 def list_supported_processes():
     process_definitions_directory = "javascript_processes"
-    implicitly_supported_processes = ["load_collection", "save_result", "reduce_dimension", "apply"]
+    implicitly_supported_processes = ["load_collection", "save_result"]
     process_definition_files = pkg_resources.resource_listdir("pg_to_evalscript", f"{process_definitions_directory}")
     supported_processes_with_files = [
         process_definition_file.replace(".js", "") for process_definition_file in process_definition_files
